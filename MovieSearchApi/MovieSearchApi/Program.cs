8d7017fbeeb79c3f53c3dd21492b48a47e136f6e
Anton <anton-kochev@users.noreply.github.com>
2025-01-16 15:48:37 +0100

    Update main_movie-adviser.yml

--- MovieSearchApi/MovieSearchApi/Program.cs
+++ MovieSearchApi/MovieSearchApi/Program.cs
@@ -22,13 +22,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureSwaggerGen(setup =>
+{
+    setup.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Movie Adviser Service",
+        Version = "v1"
+    });
+});
 
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseSwagger();
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
     app.UseSwaggerUI();
 }
 
